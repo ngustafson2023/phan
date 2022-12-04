483c37a7fc05c57042d976e4cfcba1e73694b483
Paul Ihim <paulihim@mit.edu>
2022-12-04 04:28:09 -0500

    model and collections

---
+++ server/fooditem/collection.ts
@@ -0,0 +1,112 @@
+import type { HydratedDocument, Types } from "mongoose";
+import FoodItemModel, { FoodItem } from "./model";
+
+class FoodItemCollection {
+  /**
+   * add a fooditem
+   *
+   * @return {Promise<HydratedDocument<FoodItem>> | Promise<null>} - The fooditem created
+   */
+  static async addOne(
+    foodBankId: Types.ObjectId,
+    name: String,
+    quantity: 1,
+    restrictions: []
+  ): Promise<HydratedDocument<FoodItem>[]> {
+    const foodItem = new FoodItemModel({
+      foodBankId,
+      name,
+      quantity,
+      restrictions,
+    });
+    return (await foodItem.save()).populate("FoodBank");
+  }
+
+  /**
+   * Find one fooditem
+   *
+   * @return {Promise<HydratedDocument<FoodItem> > | Promise<null>} - the matching foodItem
+   */
+  static async findOne(
+    foodBankId: Types.ObjectId,
+    name: String
+  ): Promise<HydratedDocument<FoodItem>[]> {
+    return await FoodItemModel.find({ foodBankId, name });
+  }
+
+  /**
+   * Find one fooditem by foodItemId
+   *
+   * @return {Promise<HydratedDocument<FoodItem> > | Promise<null>} - the matching foodItem
+   */
+  static async findOneById(
+    _id: Types.ObjectId
+  ): Promise<HydratedDocument<FoodItem> | null> {
+    return await FoodItemModel.findOne({ _id });
+  }
+
+  /**
+   * Find all fooditems associated with a foodbank
+   *
+   * @return {Promise<HydratedDocument<FoodItem> > | Promise<null>} - the matching foodItems
+   */
+  static async findAllByFoodBank(
+    foodBankId: Types.ObjectId
+  ): Promise<HydratedDocument<FoodItem>[]> {
+    return await FoodItemModel.find({ foodBankId });
+  }
+
+  /**
+   * Find all fooditem
+   *
+   * @return {Promise<HydratedDocument<FoodItem>[]> | Promise<null>} - all foodItems
+   */
+  static async findAll(): Promise<HydratedDocument<FoodItem>[]> {
+    return await FoodItemModel.find({});
+  }
+
+  /**
+   * updates a foodItem based on Id
+   *
+   * @return {Promise<HydratedDocument<FoodItem> > | Promise<null>} - updated foodItem
+   */
+  static async updateOneById(
+    foodItemId: Types.ObjectId,
+    name?: String,
+    quantity?: Number,
+    restrictions?: String[]
+  ): Promise<HydratedDocument<FoodItem>> {
+    const foodItem = (await FoodItemModel.findOne({ _id: foodItemId })) as any;
+
+    if (name) {
+      foodItem.name = name;
+    }
+    if (quantity) {
+      foodItem.quantity = quantity;
+    }
+    if (restrictions) {
+      foodItem.restrictions = restrictions;
+    }
+    return await foodItem.save().populate("User Slot FoodItem");
+  }
+
+  /**
+   * deletes a foodItem based on query
+   *
+   * @return {Promise<boolean>} - whether item was deleted
+   */
+  static async deleteById(foodItemId: Types.ObjectId): Promise<boolean> {
+    return (await FoodItemModel.deleteOne({ _id: foodItemId })) !== null;
+  }
+
+  /**
+   * deletes a foodItem based on query
+   *
+   * @return {Promise<number>} - deleted count
+   */
+  static async deleteMany(filter: any): Promise<number> {
+    return (await FoodItemModel.deleteMany(...filter)).deletedCount;
+  }
+}
+
+export default FoodItemCollection;
---
+++ server/order/collection.ts
@@ -0,0 +1,88 @@
+import type { HydratedDocument, Types } from "mongoose";
+import { FoodItem } from "server/fooditem/model";
+import SlotCollection from "../slot/collection";
+import OrderModel, { Order } from "./model";
+
+class OrderCollection {
+  /**
+   * add an order
+   *
+   * @return {Promise<HydratedDocument<Order>>} - The order item created
+   */
+  static async addOne(
+    userId: Types.ObjectId,
+    slotId: Types.ObjectId,
+    items: [FoodItem]
+  ): Promise<HydratedDocument<Order>> {
+    const order = new OrderModel({
+      userId,
+      slotId,
+      items,
+    });
+
+    return (await order.save()).populate("User Slot FoodItem");
+  }
+
+  /**
+   * finds an order
+   *
+   * @return {Promise<HydratedDocument<Order>>} - The order based on userId and slotId
+   */
+  static async findOneByUserAndSlot(
+    userId: Types.ObjectId,
+    slotId: Types.ObjectId
+  ): Promise<HydratedDocument<Order>[]> {
+    const order = await OrderModel.find({ userId, slotId }).populate(
+      "User Slot FoodItem"
+    );
+    return order;
+  }
+
+  /**
+   * finds all orders of a user
+   *
+   * @return {Promise<HydratedDocument<Order>[]>} - The orders of user
+   */
+  static async findAllByUser(
+    userId: Types.ObjectId
+  ): Promise<HydratedDocument<Order>[]> {
+    const order = await OrderModel.find({ userId }).populate(
+      "User Slot FoodItem"
+    );
+    return order;
+  }
+
+  /**
+   * finds all orders of a user
+   *
+   * @return {Promise<HydratedDocument<Order>>} - updated order
+   */
+  static async upateOneById(
+    orderId: Types.ObjectId,
+    slotId?: Types.ObjectId,
+    items?: [FoodItem]
+  ): Promise<HydratedDocument<Order>> {
+    const order = (await OrderModel.find({ _id: orderId })) as any;
+
+    if (slotId) {
+      order.slotId = slotId;
+    }
+
+    if (items) {
+      order.items = items;
+    }
+
+    return await order.save().populate("User Slot FoodItem");
+  }
+
+  /**
+   * deletes based on the filter
+   *
+   * @return {Promise<Number>} - deleted count
+   */
+  static async deleteByFilter(filter: any): Promise<Number> {
+    return (await OrderModel.deleteMany(...filter)).deletedCount;
+  }
+}
+
+export default OrderCollection;
