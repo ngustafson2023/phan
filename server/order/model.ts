ebd52116d26d11a730b157378e942c6cb6531114
lockehooper <86815332+lockehooper@users.noreply.github.com>
2022-12-04 23:54:26 -0500

    merge all

--- server/order/model.ts
+++ server/order/model.ts
@@ -6,14 +6,16 @@ import type { FoodItem } from "../fooditem/model";
 
 // Type definition for Order on the backend
 export type Order = {
-  _id: Types.ObjectId; // MongoDB assigns each object this ID on creation
-  userId: Types.ObjectId;
-  slotId: Types.ObjectId;
-  // items: [FoodItem];
-  items: [{
-    foodItemId: Types.ObjectId;
-    quantity: Number;
-  }];
+	_id: Types.ObjectId; // MongoDB assigns each object this ID on creation
+	userId: Types.ObjectId;
+	slotId: Types.ObjectId;
+	// items: [FoodItem];
+	items: [
+		{
+			foodItemId: Types.ObjectId;
+			quantity: Number;
+		}
+	];
 };
 
 export type PopulatedOrder = {
@@ -27,10 +29,10 @@ export type PopulatedOrder = {
 // Orders stored in this table will have these fields, with the
 // type given by the type property, inside MongoDB
 const OrderSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, required: true, ref: "User" },
-  slotId: { type: Schema.Types.ObjectId, required: true, ref: "Order" },
-  // items: [{ type: Schema.Types.ObjectId, required: true, ref: "FoodItem" }], 
-  items: { type: [{foodItemId: Schema.Types.ObjectId, quantity: Number}], required: true }
+	userId: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+	slotId: { type: Schema.Types.ObjectId, required: true, ref: "Slot" },
+	// items: [{ type: Schema.Types.ObjectId, required: true, ref: "FoodItem" }],
+	items: { type: [{ foodItemId: Schema.Types.ObjectId, quantity: Number }], required: true },
 });
 
 const OrderModel = model<Order>("Order", OrderSchema);
