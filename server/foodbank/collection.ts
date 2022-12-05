31f7471c144064fd03ac89af077a9dd7e271b508
asnowdon <66033467+asnowdon@users.noreply.github.com>
2022-12-04 19:42:36 -0500

    food bank account page

--- server/foodbank/collection.ts
+++ server/foodbank/collection.ts
@@ -4,6 +4,16 @@ import FoodBankModel from "./model";
 
 class FoodBankCollection {
 	/**
+	 * Find foodbanks info
+	 *
+	 * @param {string} foodBankId - The foodBankId of the foodbank to find
+	 * @return {Promise<HydratedDocument<FoodBank>> | Promise<null>} - The fb with the given id, if any
+	 */
+	 static async findFoodbankById(foodBankId:Types.ObjectId | string ): Promise<HydratedDocument<FoodBank>> {
+		return FoodBankModel.findOne({_id: foodBankId});
+	}	
+
+	/**
 	 * Find foodbanks with filters.
 	 *
 	 * @param {string} userId - The userId of the user to find
